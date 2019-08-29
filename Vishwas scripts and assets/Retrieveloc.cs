9baea68060139b8745eccea9dd3aca2b26217637
Vishwas G Kini <vgkini38@gmail.com>
2019-08-30 04:25:33 +1000

    Add files via upload

---
+++ Vishwas scripts and assets/Retrieveloc.cs
@@ -0,0 +1,29 @@
+﻿	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+public class Retrieveloc : MonoBehaviour
+{
+
+    public GPS locationService;
+
+    public GameObject displayText;
+
+    void Update()
+    {
+        float latitude;
+        float longitude;
+        float altitude;
+        double timestamp;
+        if (locationService.retrieveLocation(out latitude, out
+      longitude, out altitude, out timestamp))
+        {
+            displayText.GetComponent<TextMesh>().text = "Lat:   " + latitude;
+        }
+        else { 
+            displayText.GetComponent<TextMesh>().text = "No location";
+    }
+}
+            
+            }
