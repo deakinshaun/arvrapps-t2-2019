cf33bc42b748998df66700f30204b94b787ac978
Vishwas G Kini <vgkini38@gmail.com>
2019-08-30 04:28:26 +1000

    Add files via upload

---
+++ Vishwas scripts and assets/testmod.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class testmod : MonoBehaviour
+{
+    [Test]
+	public void Calculatemod_Test(){
+		Choose cs=new Choose();
+		int pr=17;
+		int ge=3;
+		int prvt=15;
+		int r=((int)(Mathf.Pow(ge,prvt))%pr);
+		int res=cs.modcalc(pr,ge,prvt);
+		Assert.That(res,Is.EqualTo(r));
+		}
+}
