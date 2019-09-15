2f3512707d6ecc67eda1ed8f14966a03916ebc13
linhtang90 <xltang@edu.deakin.au>
2019-09-15 21:34:01 +1000

    Add UI for switch between image scanning mode and 3d furniture placement mode

--- ARFurnitureSaleExperience/Assets/Scripts/AugmentedImageDisplay.cs
+++ ARFurnitureSaleExperience/Assets/Scripts/AugmentedImageDisplay.cs
@@ -5,11 +5,11 @@ using GoogleARCore;
 
 public class AugmentedImageDisplay : MonoBehaviour
 {
-    public GameObject [] displayObjectPrefabs = new GameObject[2];
-    //public List<GameObject> displayObjects = new List<GameObject>();
-    //private List<AugmentedImage> trackedImages = new List<AugmentedImage>();
-    private List<AugmentedImage> updatedTrackedImages = new List<AugmentedImage>();
-    private Dictionary<int, GameObject> m_Visualizers = new Dictionary<int, GameObject>();
+    //public GameObject [] displayObjectPrefabs = new GameObject[2];
+    public GameObject[] displayObjects = new GameObject[2];
+    private List<AugmentedImage> trackedImages = new List<AugmentedImage>();
+    //private List<AugmentedImage> updatedTrackedImages = new List<AugmentedImage>();
+    //private Dictionary<int, GameObject> m_Visualizers = new Dictionary<int, GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -24,37 +24,51 @@ public class AugmentedImageDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Session.GetTrackables<AugmentedImage>(updatedTrackedImages, TrackableQueryFilter.Updated);
-        foreach (var image in updatedTrackedImages)
-        {
-            //check if the tracked image already has a visualizer
-            GameObject visualizer = null;
-            int index = image.DatabaseIndex;
-            m_Visualizers.TryGetValue(index, out visualizer);
-            if (image.TrackingState == TrackingState.Tracking && visualizer == null)
-            {
-                // Create an anchor to ensure that ARCore keeps tracking this augmented image.
-                Anchor anchor = image.CreateAnchor(image.CenterPose);
-                visualizer = Instantiate(displayObjectPrefabs[index], anchor.transform);
-                m_Visualizers.Add(index, visualizer);
-            }
-            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
-            {
-                m_Visualizers.Remove(index);
-                GameObject.Destroy(visualizer);
-            }
-        }
-        //Session.GetTrackables<AugmentedImage>(trackedImages, TrackableQueryFilter.All);
-        //foreach (var image in trackedImages)
+        //Session.GetTrackables<AugmentedImage>(updatedTrackedImages, TrackableQueryFilter.Updated);
+        //foreach (var image in updatedTrackedImages)
         //{
-        //    if (image.TrackingState == TrackingState.Tracking)
+        //    //check if the tracked image already has a visualizer
+        //    GameObject visualizer = null;
+        //    int index = image.DatabaseIndex;
+        //    m_Visualizers.TryGetValue(index, out visualizer);
+        //    if (image.TrackingState == TrackingState.Tracking && visualizer == null)
         //    {
-        //        int index = image.DatabaseIndex;
-        //        Debug.Log("Image index" + index);
-        //        displayObjects[index].transform.position = image.CenterPose.position;
-        //        displayObjects[index].transform.rotation = image.CenterPose.rotation;
-        //        displayObjects[index].SetActive(true);
+        //        // Create an anchor to ensure that ARCore keeps tracking this augmented image.
+        //        Anchor anchor = image.CreateAnchor(image.CenterPose);
+        //        visualizer = Instantiate(displayObjectPrefabs[index], anchor.transform);
+        //        m_Visualizers.Add(index, visualizer);
+        //    }
+        //    else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+        //    {
+        //        m_Visualizers.Remove(index);
+        //        GameObject.Destroy(visualizer);
         //    }
         //}
+        Session.GetTrackables<AugmentedImage>(trackedImages, TrackableQueryFilter.Updated);
+        foreach (var image in trackedImages)
+        {
+            if (image.TrackingState == TrackingState.Tracking)
+            {
+                int index = image.DatabaseIndex;
+                Debug.Log("Image index" + index);
+                displayObjects[index].transform.position = image.CenterPose.position;
+                displayObjects[index].transform.rotation = image.CenterPose.rotation;
+                displayObjects[index].SetActive(true);
+            }
+        }
+    }
+
+    public void TurnOn()
+    {
+        transform.gameObject.SetActive(true);
+    }
+
+    public void TurnOff()
+    {
+        foreach (var displayObject in displayObjects)
+        {
+            displayObject.SetActive(false);
+        }
+        transform.gameObject.SetActive(false);
     }
 }
---
+++ ARFurnitureSaleExperience/Assets/Scripts/UIControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl: MonoBehaviour
+{
+    public GameObject imageTracker;
+    public Button imageTrackerButton;
+    public GameObject furniturePlacement;
+    public Button furniturePlacementButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToImageTracker()
+    {
+        imageTracker.GetComponent<AugmentedImageDisplay>().TurnOn();
+        imageTrackerButton.GetComponent<Image>().enabled = true;
+    }
+}
