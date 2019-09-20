9cf4bd6fa69c1d707c81c41c5ce9d6ad8885b05a
VJGediya <48464939+VJGediya@users.noreply.github.com>
2019-09-20 14:36:54 +1000

    Added ARRenovation prototype projects

---
+++ Vijay_ARRenovation/ARRenovation/Assets/Scripts/ARController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+
+#if UNITY_EDITOR
+using input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class ARController : MonoBehaviour
+{
+    private List<DetectedPlane> m_trackedPlanes = new List<DetectedPlane>();
+    public GameObject GridPrefab;
+    public GameObject Portal;
+    public GameObject ARCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //check ARCore session status
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        //following line will fill new tracked planes with the planes that arcore detected in current frame
+        Session.GetTrackables<DetectedPlane>(m_trackedPlanes, TrackableQueryFilter.New);
+
+        for (int i = 0; i < m_trackedPlanes.Count; i++)
+        {
+            GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+            grid.GetComponent<GridVisualiser>().Initialize(m_trackedPlanes[i]);
+        }
+
+        //check if user touched a screen
+        Touch touch;
+        if(Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        //check if user touched any of the tracked planes
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                TrackableHitFlags.FeaturePointWithSurfaceNormal;
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            //place the portal on top of tracked plane that touched
+            //Since portal is disabled from unity, enable through the script
+            Portal.SetActive(true);
+
+            //create a new achor
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            //ser the position of portal at hit position
+            Portal.transform.position = hit.Pose.position;
+            Portal.transform.rotation = hit.Pose.rotation;
+
+            if ((hit.Flags & TrackableHitFlags.PlaneWithinPolygon) != TrackableHitFlags.None)
+            {
+                Vector3 cameraPosition = ARCamera.transform.position;
+
+                cameraPosition.y = hit.Pose.position.y;
+
+                Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+            }
+
+            Portal.transform.parent = anchor.transform;
+        }
+
+    }
+}
---
+++ Vijay_ARRenovation/ARRenovation/Assets/Scripts/PortalManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalManager : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject sponza;
+    private Material[] sponzaMaterials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sponzaMaterials = sponza.GetComponent<Renderer>().sharedMaterials;
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+
+        if (camPositionInPortalSpace.y < 0.5f)
+        {
+            //disable stencial test
+            for(int i = 0; i < sponzaMaterials.Length; i++)
+            {
+                sponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Always);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < sponzaMaterials.Length; i++)
+            {
+                sponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+        }
+    }
+}
