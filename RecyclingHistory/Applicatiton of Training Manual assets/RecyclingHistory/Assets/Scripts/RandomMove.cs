fa32873b1377a7fcab92c0563346bd32824c752c
rahulram1996 <48549047+rahulram1996@users.noreply.github.com>
2019-09-27 13:34:06 +1000

    update

--- RecyclingHistory/Applicatiton of Training Manual assets/RecyclingHistory/Assets/Scripts/RandomMove.cs
+++ RecyclingHistory/Applicatiton of Training Manual assets/RecyclingHistory/Assets/Scripts/RandomMove.cs
@@ -23,8 +23,8 @@ public class RandomMove : MonoBehaviour
 
         x = Random.Range(-velocidadMax, velocidadMax);
         z = Random.Range(-velocidadMax, velocidadMax);
-        angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-        transform.localRotation = Quaternion.Euler(0, angulo, 0);
+      //  angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+       // transform.localRotation = Quaternion.Euler(0, angulo, 0);
     }
 
     // Update is called once per frame
@@ -36,29 +36,29 @@ public class RandomMove : MonoBehaviour
         if (transform.localPosition.x > xMax)
         {
             x = Random.Range(-velocidadMax, 0.0f);
-            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+          //  angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+          //  transform.localRotation = Quaternion.Euler(0, angulo, 0);
             tiempo = 0.0f;
         }
         if (transform.localPosition.x < xMin)
         {
             x = Random.Range(0.0f, velocidadMax);
-            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            //angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+           // transform.localRotation = Quaternion.Euler(0, angulo, 0);
             tiempo = 0.0f;
         }
         if (transform.localPosition.z > zMax)
         {
             z = Random.Range(-velocidadMax, 0.0f);
-            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+         //   angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+           // transform.localRotation = Quaternion.Euler(0, angulo, 0);
             tiempo = 0.0f;
         }
         if (transform.localPosition.z < zMin)
         {
             z = Random.Range(0.0f, velocidadMax);
-            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+            //angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+           // transform.localRotation = Quaternion.Euler(0, angulo, 0);
             tiempo = 0.0f;
         }
 
@@ -67,8 +67,8 @@ public class RandomMove : MonoBehaviour
         {
             x = Random.Range(-velocidadMax, velocidadMax);
             z = Random.Range(-velocidadMax, velocidadMax);
-            angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-            transform.localRotation = Quaternion.Euler(0, angulo, 0);
+          //  angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+           // transform.localRotation = Quaternion.Euler(0, angulo, 0);
             tiempo = 0.0f;
         }
 
