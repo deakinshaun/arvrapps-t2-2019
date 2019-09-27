41be5a7045b3d05b935c5a3327773f70c7ac0ee7
VJGediya <48464939+VJGediya@users.noreply.github.com>
2019-09-27 14:52:49 +1000

    ArchViz Final Product/Prototype

---
+++ Vijay_ArcViz_Hospital_Final_Product/ArchViz_Hospital/Assets/Scripts/ARController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+#if UNITY_EDITOR
+using input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class ARController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private List<DetectedPlane> trackedPlanes = new List<DetectedPlane>();
+    public GameObject GridPrefab;
+    public GameObject Portal;
+    public GameObject ARCamera;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        Session.GetTrackables<DetectedPlane>(trackedPlanes, TrackableQueryFilter.New);
+        for(int i=0; i < trackedPlanes.Count; ++i)
+        {
+            GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+            grid.GetComponent<GridVisualiser>().Initialize(trackedPlanes[i]);
+        }
+
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+
+        if(Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+        {
+            Portal.SetActive(true);
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            Portal.transform.position = hit.Pose.position;
+            Portal.transform.rotation = hit.Pose.rotation;
+
+            if ((hit.Flags & TrackableHitFlags.PlaneWithinPolygon) != TrackableHitFlags.None)
+            {
+                Vector3 cameraPosition = ARCamera.transform.position;
+
+                cameraPosition.y = hit.Pose.position.y;
+
+                Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+            }
+
+            Portal.transform.parent = anchor.transform;
+        }
+    }
+}
---
+++ Vijay_ArcViz_Hospital_Final_Product/ArchViz_Hospital/Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmLight : MonoBehaviour
+{
+    private float fadeSpeed = 2f;
+    private float highIntensity = 2f;
+    private float lowIntensity = 0.5f;
+    private float changeMargin = 0.2f;
+    public bool alarmOn = false;
+    private Light alarmlight;
+
+    private float targetIntensity;
+
+    void Awake()
+    {
+        alarmlight = GetComponent<Light>();
+        alarmlight.intensity = 0f;
+        targetIntensity = highIntensity;
+    }
+    void Update()
+    {
+        if (alarmOn)
+        {
+            alarmlight.intensity = Mathf.Lerp(alarmlight.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+            CheckTargetIntensity();
+        }
+        else
+        {
+            alarmlight.intensity = Mathf.Lerp(alarmlight.intensity, 0f, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    void CheckTargetIntensity()
+    {
+        if(Mathf.Abs(targetIntensity - alarmlight.intensity) < changeMargin)
+        {
+            if (targetIntensity == highIntensity)
+                targetIntensity = lowIntensity;
+            else
+                targetIntensity = highIntensity;
+        }
+    }
+}
